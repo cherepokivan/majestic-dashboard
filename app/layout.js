db32dc57ee887a21969b85b569aee963b12cf154
cherepokivan <107845995+cherepokivan@users.noreply.github.com>
2026-06-30 17:05:18 +0300

    Rename layout.js to app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+﻿import "./globals.css";
+
+export const metadata = {
+  title: "Majestic RP API Dashboard",
+  description: "Мониторинг игровых серверов Majestic",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
